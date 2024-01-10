224a6c36160a54af4d04d2135d86ea3302360565
Pedro Miotto Mujica <miottopedro@gmail.com>
2024-01-09 22:35:45 -0300

    Create 1065.py

---
+++ python/1065.py
@@ -0,0 +1,7 @@
+#Beecrowd 1065
+#Pares entre cinco números
+
+numeros = [int(input()) for i in range(5)]
+numeros_pares = [nums for nums in numeros if nums%2 == 0]
+print(f'{len(numeros_pares)} valores pares')
+#positives for positives in numeros if positives > 0
