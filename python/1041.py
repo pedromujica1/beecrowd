ccefb3f2fc32b20b35c683d6c82d89367d50804d
Pedro Miotto Mujica <miottopedro@gmail.com>
2024-01-28 23:55:46 -0300

    Update 1041.py

--- python/1041.py
+++ python/1041.py
@@ -1,1 +1,13 @@
-print("breh")
+#Coordenadas de um Ponto
+#Beecrowd 1041
+
+eixoX,eixoY = input().split()
+eixoX,eixoY= float(eixoX),float(eixoY)
+
+if eixoX == 0.0 and eixoY == 0.0:  print('Origem')
+elif eixoX > 0 and eixoY > 0: print('Q1')
+elif eixoX > 0 and eixoY < 0: print('Q4')
+elif eixoX < 0 and eixoY > 0: print('Q2')
+elif eixoX < 0 and eixoY < 0: print('Q3')
+elif eixoX == 0.0: print('Eixo Y')
+elif eixoY == 0.0: print('Eixo X')
