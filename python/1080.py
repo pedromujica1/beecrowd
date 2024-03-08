e2d5f913fc3bdcf79851cf880a268c48497e5d40
Pedro Miotto Mujica <miottopedro@gmail.com>
2024-03-07 23:18:48 -0300

    Create 1080.py

---
+++ python/1080.py
@@ -0,0 +1,10 @@
+#Maior e posição
+#Beecrowd 1080
+
+def maior_posicao(lista):
+    print(max(lista))
+    print(lista.index(max(lista)))
+    
+size = 100
+list_input = [int(input()) for i in range(3)]
+maior_posicao(list_input)
