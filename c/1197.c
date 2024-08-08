c94b2ab93ba181f20ff29fadd82f2660974c2392
pedro.mujica1 <miottopedro@gmail.com>
2024-08-07 22:08:25 -0300

    volta a facul

---
+++ c/1197.c
@@ -0,0 +1,16 @@
+//Beecrowd 1197
+//Volta à Faculdade de Física
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main(){
+    while (1){
+        int velocidade,tempo;
+        scanf("%d %d",&velocidade,&tempo);
+        printf("%d\n",(tempo*2)*velocidade);
+
+    }
+    
+
+}
